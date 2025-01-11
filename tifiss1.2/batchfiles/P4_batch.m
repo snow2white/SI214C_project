6c35de772ca981e94f73d1dcd670658f3fb2ef90
snow2white <164228644@qq.com>
2025-01-11 21:18:00 +0800

    commit info

--- tifiss1.2/batchfiles/P4_batch.m
+++
@@ -1,7 +0,0 @@
-4     % reference problem
-2     % circle obstruction 
-0.02  % mesh size
-0     % refinement level
-2     % P1/P2 switch
-
-%% Data file for punched ticket diffusion problem
--- tifiss1.2/demos/Contents.m
+++
@@ -1,5 +0,0 @@
-% DEMOS
-%
-% Files
-%   diffproblemdemo    - diffusion test problems
-%   selfadaptdemo      - adaptive refinement
